d2beb76303716ef729c0d012040566d9abe50465
you@example.com <mthao14@outlook.com>
2022-01-03 23:15:36 -0600

    removed extra logging

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -3,14 +3,11 @@ var getUserRepos = function(user) {
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
 
     // make a request to the url
-
     fetch(apiUrl).then(function(response) {
     response.json().then(function(data) {
     console.log(data);
 });
 }); 
 };
-
-console.log("outside");
 
 getUserRepos("");
